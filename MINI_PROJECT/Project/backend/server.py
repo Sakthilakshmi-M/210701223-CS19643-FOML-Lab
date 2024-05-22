32a0cb6dea4240f51e4ebca53821866eef28d3ee
Sakthilakshmi-M <sakthilakshmims@gmail.com>
2024-05-22 11:23:11 +0530

    final commit

--- MINI_PROJECT/Project/backend/server.py
+++ MINI_PROJECT/Project/backend/server.py
@@ -4,7 +4,7 @@ from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import google.generativeai as genai
 from langchain_community.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.chains.question_answering import load_qa_chain
+from langchain.chains.question_answering import load_qa_chpytain
 from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 import os
